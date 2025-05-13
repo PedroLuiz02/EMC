40d8bd2a3bfa3806d8fcbaa51eac151c83c1c4f8
PedroLuiz02 <pedroluiz0034@gmail.com>
2025-05-13 02:56:10 -0300

    V1.2 - Autenticação de Login

---
+++ assets/js/emissao.js
@@ -0,0 +1,19 @@
+const usuario = JSON.parse(localStorage.getItem('usuarioLogado'));
+
+if (usuario) {
+    document.querySelector('#loginCadastro').innerHTML = `
+        <span class="me-3">Olá, ${usuario.nome}</span>
+        <button class="btn btn-outline-danger" onclick="logout()">Logout</button>
+    `;
+}
+
+function logout() {
+    localStorage.removeItem('usuarioLogado');
+    localStorage.removeItem('token');
+    window.location.href = 'log_in.html';
+}
+
+if(localStorage.getItem('token') == null){
+  alert('você precisa estar logado para acessar essa página')
+  window.location.href = "log_in.html";
+}
