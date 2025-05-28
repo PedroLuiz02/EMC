bd96c7fa2f5343a1e02989487513ddec685b4c6f
PedroLuiz02 <pedroluiz0034@gmail.com>
2025-05-28 17:41:18 -0300

    V1.5

--- assets/js/emissao.js
+++ assets/js/emissao.js
@@ -19,11 +19,59 @@ if(localStorage.getItem('token') == null){
 }
 
 
-function insertItem(){
-  let option = document.createElement('option')
+const alunos = JSON.parse(localStorage.getItem('Aluno')) || [];
+alunos.forEach(aluno => {
+});
+
+
+function atualizarListaAluno() {
+  const select = document.getElementById("alunoSelect");
+  const alunos = JSON.parse(localStorage.getItem("Aluno")) || [];
+
+  alunos.forEach((item) => {
+    select.innerHTML += `<option value="${item.nome}">${item.nome}</option>`;
+  });
 }
+atualizarListaAluno();
+
+const curso = JSON.parse(localStorage.getItem('cursos')) || [];
+curso.forEach(aluno => {
+});
 
 
+function atualizarListaCurso() {
+  const select = document.getElementById("cursoSelect");
+  const curso = JSON.parse(localStorage.getItem("cursos")) || [];
+
+  curso.forEach((item) => {
+    select.innerHTML += `<option value="${item.nome}">${item.nome}</option>`;
+  });
+}
+atualizarListaCurso();
+
+function GerarCertificado(){
+  let nomeAluno = document.getElementById("alunoSelect").value;
+  let nomeCurso = document.getElementById("cursoSelect").value;
+  const cursos = JSON.parse(localStorage.getItem("cursos")) || [];
+  let cursoSelecionado = cursos.find(curso => curso.nome === nomeCurso);
+  let carga;
+  if (cursoSelecionado && cursoSelecionado.cargaHoraria) {
+  carga = cursoSelecionado.cargaHoraria;
+  } else {
+  carga = "carga horária indefinida";
+  }
+
+  let Box = document.querySelector("#Box");
+  Box.innerHTML = `
+  <h5>
+  Certificamos que <strong>${nomeAluno}</strong> <br>concluiu com êxito <br>o curso de <strong>${nomeCurso}</strong> <br>com carga horária de <strong>${carga}</strong>
+  </h5>`;
+
+  let BoxValid = document.querySelector("#BoxValid");
+  let CodeValidator = Math.random().toString(36).substring(2, 10).toUpperCase();
+  BoxValid.innerHTML = `<h6><strong>${CodeValidator}</strong></h6>`;
+  localStorage.setItem("CodigoValidação", CodeValidator);
+}
 
 
 
